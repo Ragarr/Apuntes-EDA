307cc99b3296f033677786d80884d4b8991e6f05
Ragarr <100472050@alumnos.uc3m.es>
2022-05-19 18:39:11 +0200

    asdasd

--- Varios/ordinaria 2020/ej1/BST.py
+++ Varios/ordinaria 2020/ej1/BST.py
@@ -98,5 +98,5 @@ class MyBST:
                 else:
                     return parent
             else:
-                return 
+                return None
                 
--- Varios/ordinaria 2020/ej3/main.py
+++ Varios/ordinaria 2020/ej3/main.py
@@ -1,5 +1,13 @@
 def getIndices(A:list,x:int):
-    if len(A)==1 and A[0]==x:
-        return 1
+    l=[]
+    getIndex(A,x,0,l)
+    return l
+
+def getIndex(A:list,x:int,cut_index,l:list):
+    if len(A) == 1 and A[0]==x:
+        l.append(cut_index)
+    elif len(A)<=1:
+        return
     else:
-        return [1+getIndices(A[:len(A)//2],x)]+[[(len(A)//2)+getIndices(A[len(A)//2:],x)]]
+        getIndex(A[:len(A)//2],x,cut_index,l)
+        getIndex(A[len(A)//2:],x,cut_index+(len(A)//2),l)
