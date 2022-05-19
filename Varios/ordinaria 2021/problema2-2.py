106c0da3162156ac9c964d2563643570eea7a8ef
Ragarr <100472050@alumnos.uc3m.es>
2022-05-19 21:31:15 +0200

    Co-authored-by: GLuis189 <GLuis189@users.noreply.github.com>

---
+++ Varios/ordinaria 2021/problema2-2.py
@@ -0,0 +1,13 @@
+from graph import Graph
+
+class MyGraph(Graph):
+    def __init__(self, vertices, directed=True):
+        super().__init__(vertices, directed)
+    
+    def closer_vertex(self,origin,visited)
+    
+    def dijkstra(self,origin):
+        distances={}
+        
+    
+    def minimumPath(self,start,end):
